ba2aeac55949ae077634b97dcda8587281452541
Ashen One <rockingbarnwal@gmail.com>
2025-05-20 18:25:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Kitty",
+    introTitle: 'Sweetheart Sannie',
+    introDesc: `Eh nigga idk what to cook,
+    but i try my best to impress you!
+    Words arent enough to describe what i feel for you
+    so am KET KET KET KETING on my keyboard instead to express to you`,
+    btnIntro: 'Click here migga',
+    title: 'Aapne kaha tha apko kabhi saccha pyar hua hi nai abhi tak 🥰',
+    desc: 'LEKIN AAP BHUL GAYE KI WO SAB BADAL GAYA JAB AAPKO MAI MILA ',
+    btnYes: 'Correct bae <33',
+    btnNo: 'ABE JNLLLLLLLL',
+    question: 'I want to talk to you over call <3',
+    btnReply: 'I love u <33',
+    reply: 'ok bc <33333333',
+    mess: 'BRUHHHHHHHHH 🥰. Baeeeeeeeeeee 😘😘',
+    messDesc: 'Respond to this only on call aight?, I will be waiting....',
+    btnAccept: 'Okiiiii hun <3',
+    messLink: 'https://t.me/notzerotwooo?text=😭😭😭😭😭' //link mess của các bạn. VD: https://t.me/notzerotwooo?text=(your number here)
+}
